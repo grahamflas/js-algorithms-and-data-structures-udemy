aeb50fa1426066e676ae45a8792d743cf76ead5d
Graham Flaspoehler <grahamflas@gmail.com>
2020-01-26 10:31:56 -0500

    Test performace of 2 implementations

--- section_2_bigO/01_timingOurCode.js
+++ section_2_bigO/01_timingOurCode.js
@@ -1,7 +1,8 @@
 // Calculate the sum of all numbers from 1 up to and including n
 
-// First pass:
-function addUpTo(n){
+// *** First solution: ***
+
+function addUpToWithLoop(n){
   let total = 0;
   for (let i=1; i<=n; i++){
     total += i;
@@ -9,4 +10,22 @@ function addUpTo(n){
   return total;
 }
 
-console.log(addUpTo(5))
+
+
+// *** Second solution: ***
+function addUpToNoLoop(n){
+  return n * (n+1) / 2;
+}
+
+
+//Test performance of both functions
+
+let t1 = performance.now()
+addUpToWithLoop(1000000000)
+let t2 = performance.now()
+console.log(`addUpToWithLoop(): ${(t2-t1) / 1000} seconds`)
+
+let t3 = performance.now()
+addUpToNoLoop(1000000000)
+let t4 = performance.now()
+console.log(`addUpToNoLoop(): ${(t4-t3) / 1000} seconds`)
