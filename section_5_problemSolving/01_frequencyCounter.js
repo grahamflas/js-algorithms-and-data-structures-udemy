3866166b6c53246d2ff35a5696ba61e5ef4d2fc5
Graham Flaspoehler <grahamflas@gmail.com>
2020-01-31 09:34:20 -0500

    Naive solution, O(n**2) due to nested loop

--- section_5_problemSolving/01_frequencyCounter.js
+++ section_5_problemSolving/01_frequencyCounter.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // The frequency of values must be the same 
 // ================================
 
-// My first pass
+// My first pass 
 function sameV1(array1, array2){
   if (array1.length === array2.length){
     for (let i=0; i<array1.length; i++){
@@ -20,4 +20,23 @@ function sameV1(array1, array2){
     console.log("The arrays are not of equal length")
     return false
   }
+} //PROBLEM: this solution is order-dependent. 
+    // Say we want [1,2,3] and [9,4,1] to return true
+
+
+
+//NAIVE SOLUTION: Time complexity is O(n^2) 
+  // We have a nested loop, which results in quadratic relationship
+  // for() loops over array1 and inside of that loop, we .indexOf() loops over array2
+
+function sameV2(array1, array2){
+  if(array1.length !== array2.length) return false
+
+  for(let i=0; i<array1.length; i++){
+    let correctIndex = array2.indexOf(array1[i] **2) //returns index of given element or -1 if element not found
+    if(correctIndex === -1) return false
+    array2.splice(correctIndex,1) //remove the correct value if it's there; start the loop over
+  }
+
+  return true
 }
