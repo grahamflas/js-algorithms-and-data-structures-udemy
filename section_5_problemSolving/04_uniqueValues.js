0d9d7b89dcc3b4755179055ff7bb8fd241a9c730
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-05 11:07:51 -0500

    First implementation, check unique values using two pointers

---
+++ section_5_problemSolving/04_uniqueValues.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let p = document.createElement("p")
+  p.innerText = "scripts loaded"
+  document.body.appendChild(p)
+});
+
+/*
+===============
+Implement a funciton countUniqueValues() that accepts a sorted array, and counts the unique values in the array. 
+There can be negative numbers in the array, but it will always be sorted
+===============
+ */
+
+//FIRST IMPLEMENTATION
+
+/*
+//Mutate the array so that uniqe values are clustered at the front
+//the final answer will be the index of i + 1
+
+                          i 
+  [1, 2,  3,  4,  5,  6,  7,  5,  5,  6,  7]  // should return 7
+                                          j 
+
+
+   //if array[i] === array[j], increment j
+   //if array[i] !== array[j]
+      // increment i 
+      // mutate array: array[i] = array[j]
+      // increment j
+
+  //return i + 1
+     
+ */
+
+function countUniqueValuesV1(array){
+  let count = 0
+  
+  if (array.length === 0){
+    return count
+  } else {
+    let i = 0
+    for (let j=1; j < array.length; j++){
+      if (array[i] !== array[j]){
+        i++;
+        array[i] = array[j]
+      }
+    }
+    count = i + 1;
+  }
+  return count
+}
+
+
+
+ 
