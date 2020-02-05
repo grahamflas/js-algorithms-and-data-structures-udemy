d52fa6db8f6265f1e098e94597224a61ebdc0ecf
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-04 19:02:02 -0500

    Refactor using for...of

--- section_5_problemSolving/02_anagramChallenge.js
+++ section_5_problemSolving/02_anagramChallenge.js
@@ -29,13 +29,11 @@ function isAnagram(str1, str2){
   let frequency1 = {};
   let frequency2 = {};
 
-  for (let i=0; i<str1.length; i++){
-    let char = str1[i]
+  for (let char of str1){
     frequency1[char] = (frequency1[char] || 0) + 1;
   }
   
-  for (let i=0; i<str2.length; i++){
-    let char = str2[i]
+  for (let char of str2){
     frequency2[char] = (frequency2[char] || 0) + 1;
   }
 
