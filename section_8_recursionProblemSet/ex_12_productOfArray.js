b589bd6c500f4abb630b9abf9e0639fbd3a6d34b
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-11 19:59:03 -0500

    Refactor - no need to delcare result = array[0]

--- section_8_recursionProblemSet/ex_12_productOfArray.js
+++ section_8_recursionProblemSet/ex_12_productOfArray.js
@@ -8,6 +8,5 @@ Write a function called productOfArray() which takes in an array of numbers and 
 
 function productOfArray(array){
   if(array.length === 0 ) return 1;
-  let result = array[0];
-  return result * productOfArray(array.slice(1));
+  return array[0] * productOfArray(array.slice(1));
 }
