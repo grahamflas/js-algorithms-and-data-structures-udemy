0b54658e85f5d82e0c673a27f67083ca72c3f3a4
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-14 08:56:50 -0500

    Fix typo

--- section_05_problemSolving/04_uniqueValues.js
+++ section_05_problemSolving/04_uniqueValues.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /*
 ===============
-Implement a funciton countUniqueValues() that accepts a sorted array, and counts the unique values in the array. 
+Implement a function countUniqueValues() that accepts a sorted array, and counts the unique values in the array. 
 There can be negative numbers in the array, but it will always be sorted
 ===============
  */
