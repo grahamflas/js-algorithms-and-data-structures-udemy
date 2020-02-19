0758d58cc0cbca2aab25934be46007ff0c7e72b5
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-19 16:06:28 -0500

    Get rid of extra variables, add test arrays

--- section_12_selectionSort/01_selectionSort.js
+++ section_12_selectionSort/01_selectionSort.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
   ul.append(li)
 });
 
+//Test arrays
+let array1 = [5, 4, 15, 14, 0, 11, 7, 2, 10, 9, 12, 3, 13, 8, 1, 6];
+let array2 = [13, 0, 11, 2, 3, 14, 8, 4, 1, 10, 6, 12, 7, 15, 5, 9];
+let array3 = [0, 2, 13, 15, 12, 10, 7, 9, 11, 3, 4, 8, 6, 14, 1, 5];
+
 function swap(array, i, j){
   [array[i], array[j]] = [array[j], array[i]]
 }
@@ -24,11 +29,9 @@ ___ return array
 
 function mySelectionSort1(array){
   for(i=0; i < array.length; i++){
-    let minimum = array[i];
     let minIndex = i;
     for (j=i+1; j < array.length; j++){
-      if (array[j] < minimum){
-        minimum = array[j];
+      if (array[j] < array[minIndex]){
         minIndex = j;
       }
     }
