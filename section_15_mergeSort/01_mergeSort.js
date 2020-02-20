4796563caf0337a5dde16f9ecc4d1ad96a57f524
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-20 12:15:35 -0500

    Visualize what happens in merge sort

--- section_15_mergeSort/01_mergeSort.js
+++ section_15_mergeSort/01_mergeSort.js
@@ -5,3 +5,24 @@ document.addEventListener("DOMContentLoaded", () => {
   ul.append(li)
 });
 
+/*
+=========================
+MERGE SORT
+=========================
++ A divide and conquer algorithm that takes advantage of the fact that arrays of length 0 || 1 are already sorted
+  + Split an array into smaller arrays and then merge them back together
+
+        [8, 3, 5, 4, 7, 6, 1, 2]
+        /                       \
+  [8, 3, 5, 4]                   [7, 6, 1, 2]
+  /           \                  /          \
+[8, 3]        [5, 4]        [7,6]           [1,2]
+/     \       /     \        /  \            /  \
+[8]    [3]   [5]     [4]    [7]  [6]        [1] [2]
+    \ /      
+    [3,8]       [4,5]         [6,7]           [1,2]
+          [3,4,5,8]                 [1,2,6,7]
+                  [1,2,3,4,5,6,7,8]     
+*/
+
+
