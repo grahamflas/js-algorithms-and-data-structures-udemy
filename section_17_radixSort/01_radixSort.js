d9c99ef4dbd18d883e6785a67976d59509636bb5
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-23 20:43:05 -0500

    Implement RadixSort

--- section_17_radixSort/01_radixSort.js
+++ section_17_radixSort/01_radixSort.js
@@ -5,6 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
   linked.append(li)
 });
 
+let a1 = [248, 45, 835, 185, 205, 916, 471, 923, 968, 607, 136, 552, 897, 511, 577];
+let a2 = [722, 385, 639, 704, 564, 549, 206, 877, 495, 284, 883, 193, 565, 454, 725];
+let a3 = [819, 70, 844, 308, 964, 612, 872, 134, 159, 801, 814, 383, 745, 909, 644];
+let a4 = [650, 743, 540, 139, 102, 873, 657, 696, 456, 358, 471, 550, 123, 252, 291];
+let a5 = [313, 520, 421, 742, 991, 912, 33, 361, 483, 141, 924, 1, 441, 263, 106];
+
+
 /*
 =========================
 RADIX SORT - HELPER FUNCTIONS
@@ -35,3 +42,31 @@ function mostDigits(array){
   return maxDigits;
 }
 
+/*
+=========================
+RADIX SORT - PSEUDO CODE
+=========================
+
+___ Define a function that accepts an array of numbers
+___ Figure out how many digits the largest number has
+___ Loop from i=0 while i < largest number of digits
+___ for each iteration
+  ___ create buckets for each digit (0 to 9)
+  ___ place each number in the corresponding bucket based on its ith digit
+___ replace existing array with values in the bucket from 0 to 9
+___ return the list at the end
+*/
+
+function radixSort(array){
+  let maxDigitCount = mostDigits(array);
+  
+  for(let i=0; i<maxDigitCount; i++){
+    let digitBuckets = Array.from ({length: 10}, () => [])
+    for(let k=0; k < array.length; k++){
+      let digit = getDigit(array[k], i);
+      digitBuckets[digit].push(array[k])
+    }
+    array = [].concat(...digitBuckets)
+  }
+  return array
+}
