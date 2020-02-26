7f897438785f2df69dff1a830e1281399612e4ca
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-26 11:50:06 -0500

    Handle SLL if length === 0

--- section_19_singlyLinkedLists/01_singlyLinkedLists.js
+++ section_19_singlyLinkedLists/01_singlyLinkedLists.js
@@ -92,6 +92,10 @@ class SinglyLinkedList {
     this.tail = newTail;
     this.tail.next = null;
     this.length--;
+    if(this.length === 0){
+      this.head = null;
+      this.tail = null;
+    }
     return current
   }
 
